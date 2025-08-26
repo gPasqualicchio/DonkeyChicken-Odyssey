e059aac9194d61efdf518d34f8445c9f06f7ef01
Innet <gianluca.pasqualicchio@gmail.com>
2025-08-26 23:59:30 +0200

    some path tile fix and restart icon

--- src/components/GameManager.tsx
+++ src/components/GameManager.tsx
@@ -5,6 +5,7 @@ import { GameState, Level, Direction } from '../game'; // Percorsi aggiornati
 import { GameAssets } from "@/config/Assets";
 import { Button } from "@/components/ui/button";
 import iconsSprite from '@/assets/icons/Icons_InGame.png';
+import restartButtonSprite from '@/assets/icons/restart_icon.png';
 
 // 1. Definiamo le props che il componente riceverà
 interface GameManagerProps {
@@ -27,7 +28,7 @@ const GameManager = ({
     <div className="relative">
       <div className="absolute top-8 right-8 flex gap-2 z-30">
         <Button onClick={handleLevelReset} className="w-12 h-12 p-0 bg-transparent hover:bg-green-700/50 border-none shadow-none">
-          <img src={iconsSprite} alt="Restart Icon" className="w-full h-full" />
+          <img src={restartButtonSprite} alt="Restart Icon" className="w-full h-full" />
         </Button>
         <Button className="w-12 h-12 p-0 bg-transparent hover:bg-green-700/50 border-none shadow-none">
           <img src={iconsSprite} alt="Main Menu Icon" className="w-full h-full" />
